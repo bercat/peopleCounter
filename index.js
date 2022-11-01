2dbe9d757ae91c8ac18ef1f3041d73c58f395840
Cat <catholm1@outlook.com>
2022-11-01 17:39:33 +0100

    small fix

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ let count = 0;
 
 let countEl = document.getElementById("count-el")
 
-
 let saveEl = document.getElementById("save-el")
 
 
 function increment() {
     count = count + 1; 
-
     countEl.textContent = count;
 } 
 
